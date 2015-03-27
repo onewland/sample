3b4c5567048d0aeb9e13015eb559f1902fd5e51e
Oliver Newland <oliver.newland@crowdflower.com>
2015-03-27 07:38:11 -0700

    strengthen user/ticket relationship

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,32 @@
+class TicketsController < ApplicationController
+  expose(:ticket) { Ticket.find(params[:id]) }
+  expose(:tickets) { Ticket.all }
+  expose(:form_action)
+
+  def new
+    self.ticket = Ticket.new
+    self.form_action = 'Create'
+  end
+
+  def create
+    ticket = Ticket.create!(ticket_params)
+    redirect_to(ticket)
+  end
+
+  def show; end
+
+  def edit
+    self.form_action = 'Update'
+    render :new
+  end
+
+  def update
+    ticket.update(ticket_params)
+    redirect_to(ticket)
+  end
+
+private
+  def ticket_params
+    params.require(:ticket).permit(:title, :description, :assignee_id)
+  end
+end
